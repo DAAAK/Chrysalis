bee1741f5ee0a166222d9b7d878236619a57a1b1
David <david.aphing-kouassi@epitech.eu>
2023-02-06 15:49:26 -0800

    contacts page

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,12 +4,14 @@ const Contact = (): JSX.Element => {
     return (
         <div className="h-screen flex justify-center items-center">
             <div className="w-3/4 h-3/4 p-6 mr-20">
-                <iframe
-                    title="Map"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.549703379306!2d-4.001277485227724!3d5.332693396131663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1eb9b4095e7f1%3A0xad1cca628972695a!2sInstitut%20Chrysalis!5e0!3m2!1sfr!2sus!4v1675725069927!5m2!1sfr!2sus"
-                    width="100%"
-                    height="100%"
-                />
+                <div className="max-w-sm m-auto">
+                    <iframe
+                        title="Map"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.549703379306!2d-4.001277485227724!3d5.332693396131663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1eb9b4095e7f1%3A0xad1cca628972695a!2sInstitut%20Chrysalis!5e0!3m2!1sfr!2sus!4v1675725069927!5m2!1sfr!2sus"
+                        width="100%"
+                        height="100%"
+                    />
+                </div>
             </div>
             <div className=" p-6 ">
                 <h2 className="text-xl font-bold mb-4">Contact Information</h2>
@@ -21,12 +23,12 @@ const Contact = (): JSX.Element => {
                     Abidjan, Côte d'Ivoire
                 </p>
                 <p className="font-bold text-lg mb-2">
-                        Phone:
-                    </p>
+                    Phone:
+                </p>
                 <p className="mb-5">+225 07 77 19 58 78</p>
                 <p className="font-bold text-lg mb-2">
-                        Email:
-                    </p>
+                    Email:
+                </p>
                 <p className="mb-5"> chrysalislaser@gmail.com</p>
             </div>
         </div>
