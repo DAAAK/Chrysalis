ca1362638b86c954efacab15d23e186e30554726
David Aphing-Kouassi <david.aphing-kouassi@epitech.eu>
2023-09-05 15:05:28 +0000

    removing unused import

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Routes, BrowserRouter, Route, HashRouter } from 'react-router-dom';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import {
   Home,
   Contacts,
